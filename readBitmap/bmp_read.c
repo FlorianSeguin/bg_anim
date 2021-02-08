75a29805b9f3ec48da5682741ab270328ca10ea9
BotFloyd <floseg82710@gmail.com>
2021-02-08 20:01:48 +0100

    Need to fix a bug occuring when setting the bg

--- readBitmap/bmp_read.c
+++ readBitmap/bmp_read.c
@@ -1,8 +1,24 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include <X11/Xlib.h>
+#include <X11/Xutil.h>
 
 int main(int argc, char **argv){
-  printf("Hello world !");
+  Display *dpy = XOpenDisplay(NULL);
+  int screen = DefaultScreen(dpy);
+  Window root = RootWindow(dpy, screen);
+  char name[] = "/home/florian/GIT/bg_anim/readBitmap/img_test.xbm";
+  Pixmap bitmap;
+  unsigned int ww, hh;
+  int hot_x, hot_y;
+  int status = XReadBitmapFile(dpy, root, name, &ww, &hh, &bitmap, &hot_x,
+      &hot_y);
+  if (status == BitmapSuccess){
+    XSetWindowBackgroundPixmap(dpy, root, bitmap);
+    XFreePixmap(dpy, bitmap);
+  } else
+    printf("This is not working \n");
+  printf("Status code : %d\n", status);
+  XClearWindow(dpy, root);
+  XCloseDisplay(dpy);
   return(0);
 }
