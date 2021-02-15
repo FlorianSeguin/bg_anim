55e4d8115581b7fae5c510c0f0312ab6158ffea7
BotFloyd <floseg82710@gmail.com>
2021-02-15 17:23:55 +0100

    Background can now be set (mosaic)

--- readBitmap/bmp_read.c
+++ readBitmap/bmp_read.c
@@ -1,24 +1,36 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
 int main(int argc, char **argv){
   Display *dpy = XOpenDisplay(NULL);
+  if (dpy == NULL){
+    printf("Connection failed\n");
+    exit(1);
+  }
   int screen = DefaultScreen(dpy);
   Window root = RootWindow(dpy, screen);
+  XWindowAttributes winattr;
+  XGCValues gc_val;
   char name[] = "/home/florian/GIT/bg_anim/readBitmap/img_test.xbm";
-  Pixmap bitmap;
   unsigned int ww, hh;
   int hot_x, hot_y;
+  Pixmap bitmap, newpix;
+  GC gc = XCreateGC(dpy, root, GCForeground|GCBackground, &gc_val);
   int status = XReadBitmapFile(dpy, root, name, &ww, &hh, &bitmap, &hot_x,
       &hot_y);
-  if (status == BitmapSuccess){
-    XSetWindowBackgroundPixmap(dpy, root, bitmap);
-    XFreePixmap(dpy, bitmap);
-  } else
+  if (status != BitmapSuccess){
     printf("This is not working \n");
-  printf("Status code : %d\n", status);
-  XClearWindow(dpy, root);
+    exit(1);
+  }
+  newpix = XCreatePixmap(dpy, root, ww, hh,
+      (unsigned int)DefaultDepth(dpy, screen));
+  XCopyPlane(dpy, bitmap, newpix, gc, 0, 0, ww, hh, 0, 0,
+      (unsigned long)1);
+  XSetWindowBackgroundPixmap(dpy, root, newpix);
+  XFreePixmap(dpy, bitmap);
   XCloseDisplay(dpy);
   return(0);
 }
